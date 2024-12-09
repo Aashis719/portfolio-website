246e4e025d314d15479b9d68c2be8ab12450f01d
Aashish Neupane <162073333+Aashis719@users.noreply.github.com>
2024-12-09 03:37:54 +0000

    responsiveness  sidebar

--- app.js
+++ app.js
@@ -11,3 +11,12 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+// for sidebar
+function showSidebar(){
+    const sidebar= document.querySelector('.sidebar')
+    sidebar.style.display ='flex'
+}
+function hideSidebar(){
+  const sidebar= document.querySelector('.sidebar')
+    sidebar.style.display ='none'
+}
